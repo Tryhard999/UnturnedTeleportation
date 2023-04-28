e5e6c5f0f67865dc941df74381161d28b3c63583
Tryhard999 <95774540+Tryhard999@users.noreply.github.com>
2023-04-28 08:29:10 +0300

    New

--- Commands/TPACommand.cs
+++ Commands/TPACommand.cs
@@ -59,4 +59,54 @@ namespace RestoreMonarchy.Teleportation.Commands
             }
         }
     }
+    
+    public class TPRCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "tpr";
+
+        public string Help => "Teleportation request command";
+
+        public string Syntax => "(player/accept/deny/cancel)";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length < 1)
+            {
+                if (!TeleportationPlugin.Instance.AcceptTPARequest((UnturnedPlayer)caller, true))
+                {
+                    UnturnedChat.Say(caller, TeleportationPlugin.Instance.Translate("TPAHelp"), TeleportationPlugin.Instance.MessageColor);
+                }
+                return;
+            }
+
+            string cmd = command[0].ToLower();
+
+            if (cmd == "accept" || cmd == "a")
+            {
+                TeleportationPlugin.Instance.AcceptTPARequest((UnturnedPlayer)caller);
+            } else if (cmd == "cancel" || cmd == "c")
+            {
+                TeleportationPlugin.Instance.CancelTPARequest((UnturnedPlayer)caller);
+            } else if (cmd == "deny" || cmd == "d")
+            {
+                TeleportationPlugin.Instance.DenyTPARequest((UnturnedPlayer)caller);
+            } else
+            {
+                var target = UnturnedPlayer.FromName(cmd);
+                if (target != null)
+                {
+                    TeleportationPlugin.Instance.SendTPARequest((UnturnedPlayer)caller, target);
+                } else
+                {
+                    UnturnedChat.Say(caller, TeleportationPlugin.Instance.Translate("TargetNotFound"), TeleportationPlugin.Instance.MessageColor);
+                }
+            }
+        }
+    }
 }
